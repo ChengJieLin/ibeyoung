21bd34cc8e525485de541cb9cd196b92496cdb23
林成杰 <linchengjie@chinasofti.com>
2023-04-03 16:41:18 +0800

    项目初始化

---
+++ pay/pay-magic-generator/src/main/java/com/ibeyoung/pay/magic/generator/PayMagicGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ibeyoung.pay.magic.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMagicGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMagicGeneratorApplication.class, args);
+	}
+
+}
---
+++ pay/pay-magic-generator/src/test/java/com/ibeyoung/pay/magic/generator/PayMagicGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibeyoung.pay.magic.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMagicGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
